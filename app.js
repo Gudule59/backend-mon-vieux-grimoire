abcfc181710090ddd96301c138eec2fdd229919f
Fabien <locwallers@gmail.com>
2024-04-29 23:30:56 +0200

    maj

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
 const express = require('express');
-
-const app = express();
-
-mongoose.connect('mongodb+srv://Fabien:openclass@cluster0.28idr2c.mongodb.net/',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
 const mongoose = require('mongoose');
 
+
+const app = express();
 
 app.use((req, res, next) => {
   console.log('Requête reçue !');
@@ -24,6 +18,5 @@ app.use((req, res, next) => {
   res.json({ message: 'Votre requête a bien été reçue !' });
   next();
 });
-
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,43 +1,54 @@
 const http = require('http');
+const mongoose = require('mongoose'); // Importez mongoose
 const app = require('./app');
 
-mongoose.connect('mongodb+srv://jimbob:<PASSWORD>@cluster0-pme76.mongodb.net/test?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
+  mongoose.connect('mongodb+srv://Fabien:openclass@cluster0-pme76.mongodb.net/test?retryWrites=true&w=majority')
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 
-const port = normalizePort(process.env.PORT || '4000');
-app.set('port', port);
-
-const errorHandler = error => {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-  const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges.');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use.');
-      process.exit(1);
-      break;
-    default:
+  const normalizePort = val => {
+    const port = parseInt(val, 10);
+  
+    if (isNaN(port)) {
+      return val;
+    }
+    if (port >= 0) {
+      return port;
+    }
+    return false;
+  };
+  const port = normalizePort(process.env.PORT || '4000');
+  app.set('port', port);
+  
+  const errorHandler = error => {
+    if (error.syscall !== 'listen') {
       throw error;
-  }
-};
-
-const server = http.createServer(app);
-
-server.on('error', errorHandler);
-server.on('listening', () => {
-  const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-  console.log('Listening on ' + bind);
-});
-
-server.listen(port);
+    }
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+    switch (error.code) {
+      case 'EACCES':
+        console.error(bind + ' requires elevated privileges.');
+        process.exit(1);
+        break;
+      case 'EADDRINUSE':
+        console.error(bind + ' is already in use.');
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+  };
+  
+  const server = http.createServer(app);
+  
+  server.on('error', errorHandler);
+  server.on('listening', () => {
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+    console.log('Listening on ' + bind);
+  });
+  
+  server.listen(port);
+  
