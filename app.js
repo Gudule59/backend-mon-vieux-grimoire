edde6c7770a6ba6ca7dae715103872634931b290
Fabien <locwallers@gmail.com>
2024-04-30 09:43:28 +0200

    maj route et controller

--- app.js
+++ app.js
@@ -1,22 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
-
+const stuffRoutes = require('./routes/stuff');
+const Thing = require('./models/thing');
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('Requête reçue !');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
 
 app.use((req, res, next) => {
   res.json({ message: 'Votre requête a bien été reçue !' });
   next();
 });
+
+app.use('/api/stuff', stuffRoutes);
 
 module.exports = app;
