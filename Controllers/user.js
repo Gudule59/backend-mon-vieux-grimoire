beed417d1b9a2701edd263ef58bcbd7a2791d5d5
Fabien <locwallers@gmail.com>
2024-04-30 10:23:41 +0200

    ajout du token aleatoire

--- Controllers/user.js
+++ Controllers/user.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 
 
 // creation user
@@ -22,16 +23,21 @@ exports.signup = (req, res, next) => {
     User.findOne({ email: req.body.email })
         .then(user => {
             if (!user) {
-                return res.status(401).json({ message: 'Paire login/mot de passe incorrecte'});
+                return res.status(401).json({ error: 'Utilisateur non trouvé !' });
             }
             bcrypt.compare(req.body.password, user.password)
                 .then(valid => {
                     if (!valid) {
-                        return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
+                        return res.status(401).json({ error: 'Mot de passe incorrect !' });
                     }
                     res.status(200).json({
                         userId: user._id,
-                        token: 'TOKEN'
+                        // creation du token suite a la config npm install jsonwebtoken
+                        token: jwt.sign( 
+                            { userId: user._id },
+                            'RANDOM_TOKEN_SECRET',
+                            { expiresIn: '24h' }
+                        )
                     });
                 })
                 .catch(error => res.status(500).json({ error }));
