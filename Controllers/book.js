756918af455fb4ee8fd7029f1b8950f4f3662bd3
Fabien <locwallers@gmail.com>
2024-05-01 19:04:09 +0200

    ajout de la fonction modification et suppression

--- Controllers/book.js
+++ Controllers/book.js
@@ -78,3 +78,45 @@ exports.rateBook = async (req, res) => {
     res.status(500).json({ message: "Erreur lors de l'ajout de l'évaluation." });
   }
 };
+
+exports.modifyBook = (req, res, next) => {
+  const bookObject = req.file ? {
+      ...JSON.parse(req.body.book),
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+  } : { ...req.body };
+
+  delete bookObject._userId;
+  Book.findOne({_id: req.params.id})
+      .then((book) => {
+          if (book.userId != req.auth.userId) {
+              res.status(401).json({ message : 'Not authorized'});
+          } else {
+              Book.updateOne({ _id: req.params.id}, { ...bookObject, _id: req.params.id})
+              .then(() => res.status(200).json({message : 'Objet modifié!'}))
+              .catch(error => res.status(401).json({ error }));
+          }
+      })
+      .catch((error) => {
+          res.status(400).json({ error });
+      });
+};
+
+ exports.deleteBook = (req, res, next) => {
+    Book.findOne({ _id: req.params.id})
+        .then(book => {
+            if (book.userId != req.auth.userId) {
+                res.status(401).json({message: 'Not authorized'});
+            } else {
+                const filename = book.imageUrl.split('/images/')[1];
+                fs.unlink(`images/${filename}`, () => {
+                    Book.deleteOne({_id: req.params.id})
+                        .then(() => { res.status(200).json({message: 'Objet supprimé !'})})
+                        .catch(error => res.status(401).json({ error }));
+                });
+            }
+        })
+        .catch( error => {
+            res.status(500).json({ error });
+        });
+ };
+
--- routes/book.js
+++ routes/book.js
@@ -8,10 +8,9 @@ const bookCtrl = require('../controllers/book');
 router.get('/', bookCtrl.getAllBook);
 //router.get('/bestrating', bookCtrl.getBestRatingBooks);
 router.get('/:id', bookCtrl.getOneBook);
-
 router.post('/', auth, multer, bookCtrl.createBook);
-router.post('/:id/rating', auth, multer, bookCtrl.rateBook); // à corriger
-//router.put('/:id', auth, multer, bookCtrl.modifyBook);
-//router.delete('/:id', auth, bookCtrl.deleteBook);
+router.post('/:id/rating', auth, multer, bookCtrl.rateBook); 
+router.put('/:id', auth, multer, bookCtrl.modifyBook);
+router.delete('/:id', auth, bookCtrl.deleteBook);
 
 module.exports = router;
